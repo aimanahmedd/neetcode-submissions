4aae64de1f03e02eb233cc62f5a24d386a8c813d
Aiman Ahmed <aimanahmedd4387@gmail.com>
2026-08-03 16:11:21 -0400

    Add: is-palindrome - submission-24

---
+++ Data Structures & Algorithms/is-palindrome/submission-24.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while right > left:
+            while not s[right].isalnum() and right > left:
+                right-=1
+            while not s[left].isalnum() and right > left:
+                left+=1
+
+            if s[right].lower() != s[left].lower():
+                return False
+
+            right-=1
+            left+=1
+
+        return True
